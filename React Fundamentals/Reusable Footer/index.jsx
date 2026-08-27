6d13c18adf274a7cd3cba65e2a45636c0e351d7e
SoftSkull111 <softskull111@gmail.com>
2026-08-27 23:39:13 +0800

    ReusableFooter Lab - Done Modified previous and current lab/workshop names

---
+++ React Fundamentals/Reusable Footer/index.jsx
@@ -0,0 +1,21 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <div className="footer">
+        <ul>
+          <li><a href="#">Fitness Dashboard</a></li>
+          <li><a href="#">Services</a></li>
+        </ul>
+        <ul>
+          <li><a href="#">Watch Videos</a></li>
+          <li><a href="#">Discord</a></li>
+        </ul>
+        <ul>
+          <li><a href="#">Privacy Policy</a></li>
+          <li><a href="#">Terms & Conditions</a></li>
+        </ul>
+      </div>
+      <p className="text">© 2024 Fitness Dashboard. All Rights Reserved.</p>
+    </footer>
+  );
+};
