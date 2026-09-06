a9c546cd342dad8b0ea2a83e4b026bbd5f7aed12
SoftSkull111 <softskull111@gmail.com>
2026-09-07 00:03:29 +0800

    ColorPicker Lab - Done

---
+++ State Hooks and Routing/ColorPicker/index.jsx
@@ -0,0 +1,13 @@
+const { useState } = React;
+
+export const ColorPicker = () => {
+  const [color, setColor] = useState("#ffffff");
+  const handleColorChange = (event) => {
+    setColor(event.target.value);
+  }
+  return (
+    <div id="color-picker-container" style={{backgroundColor: color}}>
+      <input type="color" id="color-input" onChange={handleColorChange} value={color} />
+    </div>
+  )
+};
